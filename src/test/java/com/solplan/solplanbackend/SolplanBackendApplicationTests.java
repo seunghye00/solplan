302075e9514d3e59eb2096caffe76955d6150066
Seunghye Han <tmdgp1934@gmail.com>
2025-05-29 14:48:02 +0900

    init commit

---
+++ src/test/java/com/solplan/solplanbackend/SolplanBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solplan.solplanbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolplanBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
